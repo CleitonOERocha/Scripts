f791541ba81ecb21b60463dc6cfc68ca4fc8f29e
Cleiton Rocha <cleitonotavio058@gmail.com>
2020-09-08 14:30:07 -0300

    Add files via upload

--- Decretos Covid/ETL_decretos.R
+++ Decretos Covid/ETL_decretos.R
@@ -1,16 +1,5 @@
 
-#########################################################################################################
-######################### Extração dos Decretos relacionados ao Covid-19 ################################
-#########################################################################################################
-################################ Municipal - Estadual - Federal #########################################
-#########################################################################################################
-#####   ESCRITO POR:      Cleiton Rocha - www.linkedin.com/in/cleitonoerocha/                           #
-#####   EMAIL:  cleitonrocha@sei.ba.gov.br // cleitonotavio058@gmail.com                                #
-#####   LICENÇA:          GPLv3                                                                         #
-#########################################################################################################
-#########################################################################################################
-
-
+# Pacotes
 library(dplyr)
 library(tidyr)
 library(purrr)
@@ -18,71 +7,44 @@ library(tidyverse)
 library(xml2)
 library(stringr)
 library(rvest)
+library(mgsub)
+library(gsubfn)
+library(lubridate)
 
 # altere o caminho para onde os arquivos serão salvos #
-setwd("C:\\Users\\cleitonrocha\\Desktop\\Cidacs\\ETL")
+setwd("C:\\Users\\pc\\Desktop\\Cidacs\\ETL")
 
 ########################################################
 #### DECRETOS POR ESTADO
 ########################################################
 
-Acre <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=AC", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Alagoas <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=AL", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Amapá <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=AP", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Amazonas <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=AM", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Bahia<- read.csv2("https://leismunicipais.com.br/coronavirus?estado=BA", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Ceará <-read.csv2("https://leismunicipais.com.br/coronavirus?estado=CE", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Distrito_Federal<- read.csv2("https://leismunicipais.com.br/coronavirus?estado=DF", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Espírito_Santo <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=ES", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Goiás <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=GO", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Maranhão <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=MA", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Mato_Grosso <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=MT", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Mato_Grosso_do_Sul <-read.csv2("https://leismunicipais.com.br/coronavirus?estado=MS", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Minas_Gerais <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=MG", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Pará <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=PA", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Paraíba  <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=PB", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Paraná  <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=PR", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Pernambuco <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=PE", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Piauí <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=PI", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Rio_de_Janeiro <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=RJ", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Rio_Grande_do_Norte <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=RN", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Rio_Grande_do_Sul  <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=RS", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Rondônia <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=RO", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Roraima <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=RR", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Santa_Catarina  <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=SC", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-São_Paulo <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=SP", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Sergipe <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=SE", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-Tocantins <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=TO", encoding =  'UTF-8', sep=",", stringsAsFactors = F)
-
-# criando coluna 'Estado'
-Acre <-Acre %>% mutate(Estado = "Acre")
-Alagoas <-Alagoas %>% mutate(Estado = "Alagoas")
-Amapá <- Amapá %>% mutate(Estado = "Amapá")
-Amazonas <- Amazonas %>% mutate(Estado = "Amazonas")
-Bahia <- Bahia %>% mutate(Estado = "Bahia")
-Ceará <- Ceará %>% mutate(Estado = "Ceará")
-Distrito_Federal <- Distrito_Federal %>% mutate(Estado = "Distrito Federal")
-Espírito_Santo <- Espírito_Santo %>% mutate(Estado = "Espírito Santo")
-Goiás <- Goiás %>% mutate(Estado = "Goiás")
-Maranhão <- Maranhão %>% mutate(Estado = "Maranhão")
-Mato_Grosso <- Mato_Grosso %>% mutate(Estado = "Mato Grosso")
-Mato_Grosso_do_Sul <- Mato_Grosso_do_Sul %>% mutate(Estado = "Mato Grosso do Sul")
-Minas_Gerais <- Minas_Gerais %>% mutate(Estado = "Minas Gerais")
-Pará <- Pará %>% mutate(Estado = "Pará")
-Paraíba <- Paraíba %>% mutate(Estado = "Paraíba")
-Paraná <- Paraná %>% mutate(Estado = "Paraná")
-Pernambuco <- Pernambuco %>% mutate(Estado = "Pernambuco")
-Piauí <- Piauí %>% mutate(Estado = "Piauí")
-Rio_de_Janeiro <- Rio_de_Janeiro %>% mutate(Estado = "Rio de Janeiro")
-Rio_Grande_do_Norte <- Rio_Grande_do_Norte %>% mutate(Estado = "Rio Grande do Norte")
-Rio_Grande_do_Sul <- Rio_Grande_do_Sul %>% mutate(Estado = "Rio Grande do Sul")
-Rondônia <- Rondônia %>% mutate(Estado = "Rondônia")
-Roraima <- Roraima %>% mutate(Estado = "Roraima")
-Santa_Catarina <- Santa_Catarina %>% mutate(Estado = "Santa Catarina")
-São_Paulo <- São_Paulo %>% mutate(Estado = "São Paulo")
-Sergipe <- Sergipe %>% mutate(Estado = "Sergipe")
-Tocantins <- Tocantins %>% mutate(Estado = "Tocantins")
-
+Acre <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=AC", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Acre")
+Alagoas <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=AL", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Alagoas")
+Amapá <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=AP", encoding =  'UTF-8', sep=",", stringsAsFactors = F)  %>% mutate(Estado = "Amapá")
+Amazonas <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=AM", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Amazonas")
+Bahia<- read.csv2("https://leismunicipais.com.br/coronavirus?estado=BA", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Bahia")
+Ceará <-read.csv2("https://leismunicipais.com.br/coronavirus?estado=CE", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Ceará")
+Distrito_Federal<- read.csv2("https://leismunicipais.com.br/coronavirus?estado=DF", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Distrito Federal")
+Espírito_Santo <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=ES", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Espírito Santo")
+Goiás <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=GO", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Goiás")
+Maranhão <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=MA", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Maranhão")
+Mato_Grosso <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=MT", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Mato Grosso")
+Mato_Grosso_do_Sul <-read.csv2("https://leismunicipais.com.br/coronavirus?estado=MS", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Mato Grosso do Sul")
+Minas_Gerais <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=MG", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Minas Gerais")
+Pará <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=PA", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Pará")
+Paraíba  <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=PB", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Paraíba")
+Paraná  <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=PR", encoding =  'UTF-8', sep=",", stringsAsFactors = F)  %>% mutate(Estado = "Paraná")
+Pernambuco <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=PE", encoding =  'UTF-8', sep=",", stringsAsFactors = F)  %>% mutate(Estado = "Pernambuco")
+Piauí <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=PI", encoding =  'UTF-8', sep=",", stringsAsFactors = F)  %>% mutate(Estado = "Piauí")
+Rio_de_Janeiro <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=RJ", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Rio de Janeiro")
+Rio_Grande_do_Norte <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=RN", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Rio Grande do Norte")
+Rio_Grande_do_Sul  <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=RS", encoding =  'UTF-8', sep=",", stringsAsFactors = F)  %>% mutate(Estado = "Rio Grande do Sul")
+Rondônia <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=RO", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Rondônia")
+Roraima <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=RR", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Roraima")
+Santa_Catarina  <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=SC", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Santa Catarina")
+São_Paulo <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=SP", encoding =  'UTF-8', sep=",", stringsAsFactors = F)  %>% mutate(Estado = "São Paulo")
+Sergipe <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=SE", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Sergipe")
+Tocantins <- read.csv2("https://leismunicipais.com.br/coronavirus?estado=TO", encoding =  'UTF-8', sep=",", stringsAsFactors = F) %>% mutate(Estado = "Tocantins")
 
 ####################################################
 ######## Dataset com todos os decretos, por Estado
@@ -112,9 +74,9 @@ DECRETOS_ESTADOS$Tipo <- ifelse(DECRETOS_ESTADOS$Localidade %in% c("Acre","Alago
                                 "São Paulo","Sergipe","Tocantins"), "Estadual", "Municipal")
 
 
-#############################################################
-###### Decretos Federais - Ajustando Javascript para HTML 
-#############################################################
+###################################################################
+###### Decretos Federais - Capturando página e gerando ela em HTML  
+###################################################################
 
 writeLines("var url = 'http://www.planalto.gov.br/ccivil_03/Portaria/quadro_portaria.htm';
 var page = new WebPage();
@@ -179,11 +141,13 @@ fed_covid <- fed_covid %>% mutate(Tipo="Federal") %>% mutate(Localidade="Brasil"
 Decretos_Covid_Brasil <- full_join(DECRETOS_ESTADOS, fed_covid, by=c("Decretos","Localidade","Ementa","Tipo"))
 
 #### Criando coluna 'Tipo do Documento'
-
 Decretos_Covid_Brasil$Tipo_Documento <- word(Decretos_Covid_Brasil$Decretos)
 
 ### Retirando '/' depois do nome do município 
 Decretos_Covid_Brasil$Localidade <- gsub("\\/.*","",Decretos_Covid_Brasil$Localidade)
+
+### Removendo coluna não utilizada
+Decretos_Covid_Brasil$Url <- NULL
 
 ########################################################################
 ######### Carregando dataset com códigos dos municípios
@@ -204,7 +168,35 @@ Decretos_Covid_Brasil <- left_join(Decretos_Covid_Brasil, codigos_igbe, by = c("
   
   Decretos_Covid_Brasil$Cod_UF <- ifelse(Decretos_Covid_Brasil$Tipo == "Municipal",
                                          Decretos_Covid_Brasil$Cod_UF, NA)
+  
+########################################################################
+######### Criando coluna com data dos decretos 
+#######################################################################
+  
+### criando lista com nome dos meses em português e seu respectivo número 
+  meses_pt <- c("de janeiro de",	"de fevereiro de",	"de março de",	"de abril de",
+                "de maio de",	"de junho de",	"de julho de",	"de agosto de",
+                "de setembro de",	"de outubro de",	"de novembro de",	"de dezembro de")
+  
 
+  meses_numb <- c("/01/", "/02/", "/03/", "/04/", "/05/", "/06/", "/07/", "/08/", "/09/", "/10/", "/11/", "/12/")
+
+### condicao para filtrar data na coluna - criando coluna mês
+  Decretos_Covid_Brasil$Decretos_date <- ifelse(Decretos_Covid_Brasil$Tipo %in% c("Estadual","Municipal"), 
+         mgsub::mgsub(Decretos_Covid_Brasil$Decretos, meses_pt, meses_numb),
+         strapplyc(Decretos_Covid_Brasil$Decretos, ", de \\d+.\\d+.\\d+", simplify = TRUE))
+  
+### Transformando lista em caracter
+  Decretos_Covid_Brasil$Decretos_date <- as.character(Decretos_Covid_Brasil$Decretos_date)
+
+### Criando condição para extrair datas de acordo com seu padrão
+  Decretos_Covid_Brasil$Decretos_date <- ifelse(Decretos_Covid_Brasil$Tipo %in% c("Estadual","Municipal"),
+                                               strapplyc(Decretos_Covid_Brasil$Decretos_date, "\\d+ /\\d+/ \\d+", simplify = TRUE), 
+                                               Decretos_Covid_Brasil$Decretos_date)
+
+### Convertendo coluna para formato 'Date'
+  Decretos_Covid_Brasil$Decretos_date <- dmy(Decretos_Covid_Brasil$Decretos_date)
+  
 ############################################################
 ############# Dataset com todos os decretos em Salvador
 ############################################################
@@ -216,7 +208,7 @@ DECRETOS_SALVADOR <- Decretos_Covid_Brasil %>% filter(Localidade == "Salvador")
 ############################################################################################
 
 DECRETOS_BAHIA_ESTADUAL <- Decretos_Covid_Brasil %>% filter(Localidade == "Bahia") %>% 
-                           select(Localidade, Decretos, Ementa, Tipo, Url, Tipo_Documento) # Decretos Estaduais
+                           select(Localidade, Decretos, Ementa, Tipo, Tipo_Documento) # Decretos Estaduais
 
 DECRETOS_BAHIA_TODOS <- Decretos_Covid_Brasil %>% filter(Estado == "Bahia") # Decretos do Estado e Municipios
 
@@ -239,3 +231,5 @@ write.csv2(DECRETOS_FEDERAIS,"Decretos_Federais.csv", row.names = F) # Federais
 
 # removendo datasets não utilizados #
 rm(html2, html_tabela, fed_covid)
+
+
